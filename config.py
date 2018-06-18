59d1832c345ba553394273d2c2a783ae9a1f3165
Samuel Pecár <samuel.pecar@hotmail.com>
2018-06-18 15:17:46 +0200

    config changes

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 epochs = 20
 batch_size = 64
 dim = 200
-embeddings_path = 'data/glove.twitter.27B.200d.txt'
+embeddings_path = 'data/emb.200d.txt'
 
 emoji2word = False
 
 # Partition of train dataset
-partition = 10000
-# partition = None
+# partition = 10000
+partition = None
 
 
 # Output formatting
@@ -16,10 +16,10 @@ partition = 10000
 # verbose = 0
 
 # for progess bar
-# verbose = 1
+verbose = 1
 
 # for one line per epoch
-verbose = 2
+# verbose = 2
 
 # Early stopping
 early_stop_monitor = 'val_f1'
